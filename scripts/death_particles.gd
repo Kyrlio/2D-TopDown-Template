a9b05013e313cf94e8e54887682ccbe7fc31eb22
Cédric Roussel <cedricroussel2002@gmail.com>
2025-08-12 14:23:09 +0200

    Added dash

--- scripts/death_particles.gd
+++ scripts/death_particles.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func death_particles():
 	$GPUParticles2D.emitting = true
+
+
+func _on_gpu_particles_2d_finished() -> void:
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,9 +12,24 @@ var current_look_dir = "right"
 @onready var sword: Sprite2D = $Sprite2D/Sword
 @onready var sword_anim: AnimationPlayer = $Sprite2D/Sword/SwordAnim
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var run_particles: GPUParticles2D = $RunParticles
 
 @export var health: float = 3.0
 var is_hurt: bool = false
+
+# --- MOVEMENTS ---
+var input: Vector2 = Vector2.ZERO
+var active: bool = true
+
+# Dashing
+var is_dashing: bool = false
+var dash_start_position: Vector2 = Vector2.ZERO
+var dash_direction: Vector2 = Vector2.ZERO
+var dash_timer : float = 0.0
+@export var dash_speed: float = 200.0
+@export var dash_max_distance: float = 70.0
+@export var dash_curve: Curve
+@export var dash_cooldown: float = 1
 
 # --- ATTACKS ---
 var can_slash: bool = true
@@ -26,6 +41,7 @@ var knockback: Vector2 = Vector2.ZERO
 var knockback_timer: float = 0.0
 
 
+
 func _physics_process(delta: float) -> void:
 	if knockback_timer > 0.0:
 		velocity = knockback
@@ -33,7 +49,7 @@ func _physics_process(delta: float) -> void:
 		if knockback_timer <= 0.0:
 			knockback = Vector2.ZERO
 	else:
-		if not is_hurt:
+		if not is_hurt and not is_dashing:
 			move(delta)
 	# --- ATTACK ---
 	if Input.is_action_pressed("attack") and can_slash:
@@ -41,22 +57,58 @@ func _physics_process(delta: float) -> void:
 		sword_anim.play("slash")
 		can_slash = false
 	
-	update_look() 
+	# --- DASH ---
+	if Input.is_action_just_pressed("dash") and not is_hurt and input and dash_timer <= 0:
+		is_dashing = true
+		dash_start_position = position
+		dash_direction = input
+		dash_timer = dash_cooldown
+		animation_player.play("dash")
+		var dash_duration = dash_max_distance / dash_speed
+		var animation_length = animation_player.get_animation("dash").length
+		animation_player.speed_scale =  animation_length / dash_duration
+		#print("dash duration: ", dash_duration, " | animation length: ", animation_length, " | animation speed scale: ", animation_player.speed_scale)
+	
+	
+	update_look()
+	dash(delta)
 	move_and_slide()
 
 
+func dash(delta):
+	# Perform dash
+	if is_dashing:
+		var current_distance = position.distance_to(dash_start_position)
+		#print("dash? ", is_dashing, " | pos: ", global_position, " | start: ", dash_start_position, " | dist: ", current_distance)
+		if current_distance >= dash_max_distance or is_on_wall():
+			not_dashing()
+		else:
+			velocity = dash_direction * dash_speed 
+	
+	# Reduces dash timer
+	if dash_timer > 0:
+		dash_timer -= delta
+
+func not_dashing():
+	if not is_dashing:
+		return
+	else:
+		is_dashing = false
+
 func move(delta: float):
 	# --- MOVEMENTS ---
-	var input = Vector2(
+	input = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	).normalized()
 	
 	if input:
+		run_particles.emitting = true
 		animation_player.play("walk")
 		#$Sprite2D.flip_h = true if input.x < 0 else false
 		animation_player.speed_scale = (velocity/MAX_SPEED).distance_to(Vector2.ZERO) + 0.1
 	else:
+		run_particles.emitting = false
 		animation_player.speed_scale = 0.75
 		if not is_hurt:
 			animation_player.stop()
@@ -102,6 +154,8 @@ func apply_knockback(direction: Vector2, force: float, knockback_duration: float
 
 func take_damage(damage: float):
 	if not is_hurt:
+		if is_dashing:
+			not_dashing()
 		health -= damage
 		print("life : " + str(health))
 		
@@ -115,5 +169,7 @@ func take_damage(damage: float):
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "hurt":
-		is_hurt = false
+	match anim_name:
+		"hurt": is_hurt = false
+		"dash": not_dashing()
+		_: return
